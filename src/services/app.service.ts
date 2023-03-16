1eae28baaf94f654c5ce60316fd945ba9d9e0fc4
Kakaru_0 <81516330+bvnm0121@users.noreply.github.com>
2023-03-16 20:48:56 +0900

    [commit]
    This is the base Nest.js structure I'm using!

---
+++ src/services/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { IsNotEmpty } from 'class-validator'
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
